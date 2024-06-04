a7cdac5081f81315850f7bb8ab3af6e58e935261
Pantouvakis <padouvas13@gmail.com>
2024-06-04 16:51:07 +0300

    fixed highlighting in browsing

--- frontend/src/Pages/Browsing/Browsing.jsx
+++ frontend/src/Pages/Browsing/Browsing.jsx
@@ -9,6 +9,7 @@ function Browsing() {
   const [tableData, setTableData] = useState([]);
   const [connectionVocData, setConnectionVocData] = useState([]);
   const [vocData, setVocData] = useState({});
+  const [selectedID, setSelectedID] = useState('');
 
   useEffect(() => {
     async function fetchTables() {
@@ -45,14 +46,27 @@ function Browsing() {
 
   const handleSelectTable = (table) => {
     setSelectedTable(table);
-    setSelectedEntity(table); // Update selected entity to reflect the selection
+    setSelectedEntity(table);
+    setSelectedID(''); // Reset selectedID when changing tables
   };
 
-  const handleEntityClick = (tableName) => {
+  const handleEntityClick = async (tableName, id) => {
     setSelectedTable(tableName);
     setSelectedEntity(tableName);
+    setSelectedID(id);
+    
+    try {
+      const tableResponse = await axios.post('http://localhost:5000/read-data', { tableName });
+      const connectionVocResponse = await axios.get(`http://localhost:5000/get-connectionvoc/${tableName}`);
+      setTableData(tableResponse.data.data);
+      setConnectionVocData(connectionVocResponse.data);
+    } catch (error) {
+      console.error('Error fetching table data:', error);
+      setTableData([]);
+      setConnectionVocData([]);
+    }
   };
-
+  
   return (
     <div style={{ marginBottom: '20px', paddingTop: '50px', paddingLeft: '10px', gap: '10px' }}>
       <h1>Browsing</h1>
@@ -77,12 +91,11 @@ function Browsing() {
             </thead>
             <tbody>
               {tableData.map((row, rowIndex) => (
-                <tr key={rowIndex}>
+                <tr key={rowIndex} style={selectedID === row['ID'] ? { backgroundColor: 'yellow' } : {}}>
                   {Object.entries(row).map(([key, value], colIndex) => {
                     const entry = connectionVocData.find(entry => entry.tableC === key);
                     const isEntity = entry && entry.vocS === 2;
                     const isVocabulary = entry && entry.vocS === 1;
-                    const isSelected = selectedTable === selectedEntity;
 
                     let displayValue = value;
                     let style = {};
@@ -97,9 +110,9 @@ function Browsing() {
                       <td 
                         key={colIndex} 
                         style={style} 
-                        onClick={() => isEntity && handleEntityClick(entry.vocT)}
+                        onClick={() => isEntity && handleEntityClick(entry.vocT, value)}
                       >
-                        {displayValue}
+                        {displayValue } 
                       </td>
                     );
                   })}
