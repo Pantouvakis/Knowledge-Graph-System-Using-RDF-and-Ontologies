272b27e143077cde926dd163bca634c7fdf56200
Pantouvakis <padouvas13@gmail.com>
2024-06-28 16:49:38 +0300

    fixed list of doc ent a bug

--- frontend/src/Pages/Documentation/ListOfDocumentedEntities.jsx
+++ frontend/src/Pages/Documentation/ListOfDocumentedEntities.jsx
@@ -27,14 +27,11 @@ function ListOfDocumentationEntities() {
         setLoading(false);
       }
     };
-
     fetchTables();
   }, []);
 
   const fetchVocInsertions = useCallback(async (tableName) => {
-    if (tableName === "Remove") {
-      return null;
-    }
+    if (tableName === "Remove") return null;
     try {
       const response = await axios.post('http://localhost:5000/read-names-vdata', { tableName });
       return Array.isArray(response.data) ? response.data : [];
@@ -45,15 +42,11 @@ function ListOfDocumentationEntities() {
   }, []);
 
   const fetchEntityInsertions = useCallback(async (tableName) => {
-    if (tableName === "Remove") {
-      return null;
-    }
+    if (tableName === "Remove") return null;
     try {
       const response = await axios.post('http://localhost:5000/read-data', { tableName });
       const { data } = response.data;
-      return Array.isArray(data)
-        ? data.map(entry => ({ id: entry.ID, display: Object.values(entry).slice(1).join('-') }))
-        : [];
+      return Array.isArray(data) ? data.map(entry => ({ id: entry.ID, display: Object.values(entry).slice(1).join('-') })) : [];
     } catch (error) {
       setMessage(`Error fetching entity insertions for table "${tableName}"`);
       return [];
@@ -68,7 +61,6 @@ function ListOfDocumentationEntities() {
         axios.post('http://localhost:5000/read-data', { tableName: table }),
         axios.get(`http://localhost:5000/get-connectionvoc/${table}`)
       ]);
-
       setTableData(tableResponse.data.data);
       setConnectionVocData(connectionVocResponse.data);
 
@@ -103,19 +95,13 @@ function ListOfDocumentationEntities() {
         if (entry.vocS === 1) {
           const vocInsertions = await fetchVocInsertions(entry.vocT);
           vocOptionsTemp[key] = vocInsertions;
-
           const matchedOption = vocInsertions.find(option => option.ID === value);
-          if (matchedOption) {
-            vocDataTemp[value] = matchedOption.name;
-          }
+          if (matchedOption) vocDataTemp[value] = matchedOption.name;
         } else if (entry.vocS === 2) {
           const entityInsertions = await fetchEntityInsertions(entry.vocT);
           vocOptionsTemp[key] = entityInsertions;
-
           const matchedOption = entityInsertions.find(option => option.id === value);
-          if (matchedOption) {
-            vocDataTemp[value] = matchedOption.display;
-          }
+          if (matchedOption) vocDataTemp[value] = matchedOption.display;
         }
       }
     }
@@ -132,7 +118,7 @@ function ListOfDocumentationEntities() {
     try {
       const rowId = tableData[rowIndex].ID;
       let updatedRowData = { ...editingRowData };
-  
+
       for (const key in updatedRowData) {
         if (updatedRowData[key] === '') {
           updatedRowData[key] = null;
@@ -142,16 +128,16 @@ function ListOfDocumentationEntities() {
           }
         }
       }
-  
+
       await axios.post('http://localhost:5000/update-data', {
         tableName: selectedTable,
         newData: updatedRowData,
         id: rowId,
       });
-  
+
       const updatedTableData = [...tableData];
       updatedTableData[rowIndex] = { ...updatedTableData[rowIndex], ...updatedRowData };
-  
+
       const updatedVocData = { ...vocData };
       for (const [key, value] of Object.entries(updatedRowData)) {
         const entry = connectionVocData.find(entry => entry.tableC === key);
@@ -163,94 +149,90 @@ function ListOfDocumentationEntities() {
           }
         }
       }
-  
+
       setVocData(updatedVocData);
       setTableData(updatedTableData);
       setEditingRowIndex(null);
       setMessage('Row saved successfully');
+      
+      await handleSelectTable(selectedTable);
     } catch (error) {
       setMessage('Error saving edit');
     }
-  }, [connectionVocData, editingRowData, selectedTable, tableData, vocData, vocOptions]);
-  
-  
-  
+  }, [connectionVocData, editingRowData, handleSelectTable, selectedTable, tableData, vocData, vocOptions]);
 
   const handleDelete = useCallback(async (rowIndex) => {
     try {
       const rowId = tableData[rowIndex].ID;
       await axios.delete(`http://localhost:5000/delete2-row/${selectedTable}/${rowId}`);
-
-      const updatedTableData = tableData.filter((_, index) => index !== rowIndex);
-      setTableData(updatedTableData);
-
+      setTableData(tableData.filter((_, index) => index !== rowIndex));
       setMessage('Row deleted successfully');
+      await handleSelectTable(selectedTable);
     } catch (error) {
       setMessage('Error deleting row');
     }
-  }, [selectedTable, tableData]);
+  }, [handleSelectTable, selectedTable, tableData]);
 
-const renderCellContent = useCallback((key, value, rowIndex) => {
-  const entry = connectionVocData.find(entry => entry.tableC === key);
-  const isEntity = entry?.vocS === 2;
-  const isVocabulary = entry?.vocS === 1;
+  const renderCellContent = useCallback((key, value, rowIndex) => {
+    const entry = connectionVocData.find(entry => entry.tableC === key);
+    const isEntity = entry?.vocS === 2;
+    const isVocabulary = entry?.vocS === 1;
 
-  let displayValue = value;
-  let style = {};
-  if (isEntity) {
-    style = { color: 'blue' };
-    displayValue = vocOptions[key]?.find(option => option.id === value)?.display || value;
-  } else if (isVocabulary) {
-    style = { color: 'red' };
-    displayValue = vocData[value] || value;
-  }
-
-  if (value === null) {
-    style.backgroundColor = 'lightgrey';
-  }
-
-  if (editingRowIndex === rowIndex && key !== 'ID') {
-    if (isVocabulary) {
-      return (
-        <select
-          value={editingRowData[key] ?? ''}
-          onChange={(e) => handleInputChange(e, key)}
-        >
-          <option value="">Select</option>
-          {(vocOptions[key] || []).map(option => (
-            <option key={option.ID} value={option.ID}>{option.name}</option>
-          ))}
-        </select>
-      );
-    } else if (isEntity) {
-      return (
-        <select
-          value={editingRowData[key] ?? ''}
-          onChange={(e) => handleInputChange(e, key)}
-        >
-          <option value="">Select</option>
-          {(vocOptions[key] || []).map(option => (
-            <option key={option.id} value={option.id}>
-              {option.display}
-            </option>
-          ))}
-        </select>
-      );
-    } else {
-      return (
-        <input
-          placeholder="Enter text"
-          type="text"
-          value={editingRowData[key] ?? ''}
-          onChange={(e) => handleInputChange(e, key)}
-        />
-      );
+    let displayValue = value;
+    let style = {};
+    if (isEntity) {
+      style = { color: 'blue' };
+      displayValue = vocOptions[key]?.find(option => option.id === value)?.display || value;
+    } else if (isVocabulary) {
+      style = { color: 'red' };
+      displayValue = vocData[value] || value;
     }
-  } else {
-    return displayValue !== null ? displayValue : '';
-  }
-}, [connectionVocData, editingRowData, editingRowIndex, handleInputChange, vocData, vocOptions]);
 
+    if (value === null) {
+      style.backgroundColor = 'lightgrey';
+    }
+
+    if (editingRowIndex === rowIndex && key !== 'ID') {
+      if (isVocabulary) {
+        return (
+          <select
+            value={editingRowData[key] ?? ''}
+            onChange={(e) => handleInputChange(e, key)}
+          >
+            <option value="">Select</option>
+            {(vocOptions[key] || []).map(option => (
+              <option key={option.ID} value={option.ID}>{option.name}</option>
+            ))}
+          </select>
+        );
+      } else if (isEntity) {
+        return (
+          <select
+            value={editingRowData[key] ?? ''}
+            onChange={(e) => handleInputChange(e, key)}
+          >
+            <option value="">Select</option>
+            {(vocOptions[key] || []).map(option => (
+              <option key={option.id} value={option.id}>
+                {option.display}
+              </option>
+            ))}
+          </select>
+        );
+      } else {
+        return (
+          <input
+            placeholder="Enter text"
+            type="text"
+            value={editingRowData[key] ?? ''}
+            onChange={(e) => handleInputChange(e, key)}
+          />
+        );
+      }
+    } else {
+      return displayValue !== null ? displayValue : '';
+    }
+  }, [connectionVocData, editingRowData, editingRowIndex, handleInputChange, vocData, vocOptions]);
 
   return (
     <div style={{ marginBottom: '20px', paddingTop: '50px', paddingLeft: '10px', gap: '10px' }}>
