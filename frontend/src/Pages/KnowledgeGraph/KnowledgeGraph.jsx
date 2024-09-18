f1f34526dad874dd90f7e3233d1867e34ac7fb08
Pantouvakis <padouvas13@gmail.com>
2024-09-19 00:10:25 +0300

    1809

--- frontend/src/Pages/KnowledgeGraph/KnowledgeGraph.jsx
+++ frontend/src/Pages/KnowledgeGraph/KnowledgeGraph.jsx
@@ -25,58 +25,62 @@ const KnowledgeGraph = () => {
     let rdfContent = '';
 
     entities.forEach((entity) => {
-      entity.data.forEach((row) => {
-        const ontologyClass = getOntologyClass(entity.name, ontologies);
-        const subject = `<${generateUri(uriPrefix, entity.name, row.ID)}>`;
+        entity.data.forEach((row) => {
+            const ontologyClass = getOntologyClass(entity.name, ontologies);
+            const subject = `<${generateUri(uriPrefix, entity.name, row.ID)}>`; 
 
-        Object.entries(row).forEach(([property, value]) => {
-          const connection = getConnection(entity.name, property, connectionVocData);
-          const valueId = value?.ID || value;
-          const predicate = property === "ID" ? "a" : `<${getPredicateUri(property, entity.name, uriMappings)}>`;
-          let object;
+            Object.entries(row).forEach(([property, value]) => {
+                const connection = getConnection(entity.name, property, connectionVocData);
+                const valueId = value?.ID || value;
+                const predicate = property === "ID" ? "a" : `<${getPredicateUri(property, entity.name, uriMappings)}>`;
+                let object;
 
-          if (property === "ID") {
-            object = ontologyClass ? `<${ontologyClass}>` : "No ontology class found";
-          } else if (connection) {
-            object = getRdfTypeObject(value, connection, uriPrefix);
-          } else {
-            object = typeof value === 'string' && value.startsWith('http') ? `<${value}>` : `"${value}"`;
-          }
+                if (property === "ID") {
+                    object = ontologyClass ? `<${ontologyClass}>` : null;  // No ontology class found
+                } else if (connection) {
+                    object = getRdfTypeObject(value, connection, uriPrefix);
+                } else {
+                    object = typeof value === 'string' && value.startsWith('http') ? `<${value}>` : `"${value}"`;
+                }
 
-          rdfContent += `${subject} ${predicate} ${object} .\n`;
+                // Check if object is not null before adding the triple
+                if (object !== '"null"') {
+                    rdfContent += `${subject} ${predicate} ${object} .\n`;
+                }
 
-          if (property === "ID") {
-            rdfContent += getOntologyTriples(subject, ontologies.filter(ont => ont.category.toLowerCase() === entity.name.toLowerCase()));
-          }
+                if (property === "ID" && object !== null) {
+                    rdfContent += getOntologyTriples(subject, ontologies.filter(ont => ont.category.toLowerCase() === entity.name.toLowerCase()));
+                }
+            });
+
+            if (vocabularyTables.includes(entity.name)) {
+                const insertionSubject = `<${uriPrefix}${entity.name}/insertion>`;
+                const insertionPredicate = "a";
+                const insertionObject = "<http://www.w3.org/2004/02/skos/core#Concept>";
+
+                rdfContent += `${insertionSubject} ${insertionPredicate} ${insertionObject} .\n`;
+            }
         });
-
-        if (vocabularyTables.includes(entity.name)) {
-          const insertionSubject = `<${uriPrefix}${entity.name}/insertion>`;
-          const insertionPredicate = "a";
-          const insertionObject = "<http://www.w3.org/2004/02/skos/core#Concept>";
-
-          rdfContent += `${insertionSubject} ${insertionPredicate} ${insertionObject} .\n`;
-        }
-      });
     });
 
     Object.entries(vocabularyData).forEach(([tableName, tableData]) => {
-      tableData.forEach((row) => {
-        const subject = `<${uriPrefix}${tableName.toLowerCase()}/${row.name}>`;
-        rdfContent += `${subject} a <http://www.w3.org/2004/02/skos/core#Concept> .\n`;
-        rdfContent += `${subject} <http://www.w3.org/2004/02/skos/core#prefLabel> "${row.name}" .\n`;
+        tableData.forEach((row) => {
+            const subject = `<${uriPrefix}${tableName.toLowerCase()}/${row.name}>`;
+            rdfContent += `${subject} a <http://www.w3.org/2004/02/skos/core#Concept> .\n`;
+            rdfContent += `${subject} <http://www.w3.org/2004/02/skos/core#prefLabel> "${row.name}" .\n`;
 
-        if (row.broader) {
-          const broaderSubject = `<${uriPrefix}${tableName.toLowerCase()}/${row.broader.replace(" ", "_")}>`;
-          rdfContent += `${subject} <http://www.w3.org/2004/02/skos/core#broader> ${broaderSubject} .\n`;
-          rdfContent += `${broaderSubject} <http://www.w3.org/2004/02/skos/core#prefLabel> "${row.broader}" .\n`;
-        }
-      });
+            if (row.broader) {
+                const broaderSubject = `<${uriPrefix}${tableName.toLowerCase()}/${row.broader.replace(" ", "_")}>`;
+                rdfContent += `${subject} <http://www.w3.org/2004/02/skos/core#broader> ${broaderSubject} .\n`;
+                rdfContent += `${broaderSubject} <http://www.w3.org/2004/02/skos/core#prefLabel> "${row.broader}" .\n`;
+            }
+        });
     });
 
     setRdfContent(rdfContent);
     return rdfContent;
-  };
+};
+
 
   useEffect(() => {
     const fetchData = async () => {
